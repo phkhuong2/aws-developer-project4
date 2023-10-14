9a10d374b85d98243bbd80547350c2cd02595edd
phkhuong <ph.khuong2@gmail.com>
2023-10-15 00:04:34 +0700

    update

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,11 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = ''
+const apiId = '55fivx7n4f'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: 'dev-w8qwnvbdyoi67dln.us.auth0.com',            // Auth0 domain
-  clientId: 'NxCQFQBhGRQ2v2fCYyL53I1oyVVZD0mA',          // Auth0 client id
+  domain: 'dev-jfiq6mi0lp3fn42u.us.auth0.com',            // Auth0 domain
+  clientId: 'b6yGDs0uPR0JL7RHLjvWpGSUVQLYGY53',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
